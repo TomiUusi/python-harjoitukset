b85cd6b3db9a2f01065d6fca2c2d87081a75744b
Tomi Uusitalo <tomi.T.uusitalo@metropolia.fi>
2026-08-19 11:26:27 +0300

    Lisätty mod03

---
+++ mod03/moi.py
@@ -0,0 +1,22 @@
+
+teksti = "tämä on merkkijono"
+
+luku = input('anna 1. luku: ')
+luku2 = input('anna 2. luku: ')
+
+luku = float(luku) # esim '10.5' -> 10.5
+luku2 = float(luku2)
+
+summa = luku + luku2
+
+#luku = 10
+#luku2 = 12.3
+
+#summa = luku + luku2
+
+#summastr = str(summa)
+#print('summa: ' + summastr)
+
+#print('lukujen ' + str(luku) + ' ja ' str(luku2) + ' summa on ' + summa + '.')
+print("lukujen", luku, "ja", luku2, "summa on", summa)
+
